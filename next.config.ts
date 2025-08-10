f402dba22dd8bf1bd0788bbee96f8a7cc35777bd
Jorge Varela <jcvarela@live.com>
2025-08-10 11:50:26 -0400

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // export to plain static files for Azure Static Web Apps
+  output: "export",
+  images: {
+    // needed for pure static export
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
